669a94da288032f58d9f890b2450e026320b9d42
amr <amr.elkady153@gmail.com>
2018-05-30 02:26:37 +0200

    nom.2

---
+++ README.md
@@ -0,0 +1,37 @@
+# What is this?:
+this is a movie website that provides you with info about movies (title,storyline,poster,trailer)
+## Requirements:
+installed python on your computer you can download it [here](https://www.python.org/downloads/release/python-2715/)
+## How to open the website:
+#### using **cmd**
+* open cmd 
+* write `cd [/D] [drive name:]\ [path]`
+* **notes :** `path` is the path to the movie folder 
+		    	don't write  `[]` in the cmd these are for demonstration
+
+* once you are in the movie directory path type `python entertainment_center.py`  
+the website should be opened,and a file named **fresh_tomatoes.html** will be added to the directory(you can open the website from it using your browser)
+## License
+MIT License
+
+Copyright (c) [2018] [amr elkady]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+#### if you need help with something or have any suggestions don't hesitate to contact me at _amr.elkady153@gmail.com_ 
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,33 +2,29 @@ import media
 import fresh_tomatoes
 
 
-# movies and their information
+# movies and their information (name,storyline,posterimage,trailer)
 interstellar = media.Movie("Interstellar",
                            "a mission to search for a newhome in space",
-                           "http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mk\
-                            er2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",
+                           "http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",  # NOQA
                            "https://www.youtube.com/watch?v=zSWdZVtXT7E")
 
 black_panther = media.Movie("Black panther",
                             "a story of a knig trying toprotect his home",
-                            "http://t1.gstatic.com/images?q=tbn:ANd9GcQPpcK\
-                             Q9eWZGxJe6eXyCW91eayLVm4KqruvJz3GP0F2T2w46yKZ",
+                            "http://t1.gstatic.com/images?q=tbn:ANd9GcQPpcKQ9eWZGxJe6eXyCW91eayLVm4KqruvJz3GP0F2T2w46yKZ",  # NOQA
                             "https://www.youtube.com/watch?v=xjDjIWPwcPU")
 
-get_out = media.Movie("Get out",
-                      "a young man observe strange things onhis trip",
-                      "https://movies.universalpictures.com/media/get-out-main-one-sheet-58753d5968ead-1.png",
-                      "https://www.youtube.com/watch?v=sRfnevzM9kQ")
+django_unchained = media.Movie("Django Unchained",
+                      "with the help of his mentor a slave trned into bounty\
+                       hunter and sets off to rescue his wife",
+                      "http://t3.gstatic.com/images?q=tbn:ANd9GcSnm2FczCxSnt69XUZqqI5-sfy66SvjiV0du9mfUKRRCGqVAurt",  # NOQA
+                      "https://www.youtube.com/watch?v=eUdM9vrCbow")
 
 shawshank_redemption = media.Movie("The shawshank redemption",
                                    "Two imprisoned men bond over a number of\
                                     years, finding solace and eventual \
                                     redemption through acts of common decency",
-                                   "http://t0.gstatic.com/images?q=tbn:\
-                                   ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_\
-                                   mImChPuMrunA1XjNTSKm",
-                                   "https://www.youtube.com/watch?v=\
-                                   6hB3S9bIaco")
+                                   "http://t0.gstatic.com/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",  # NOQA
+                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")  # NOQA
 
 
 passengers = media.Movie("passengers",
@@ -39,5 +35,7 @@ passengers = media.Movie("passengers",
                          "https://www.youtube.com/watch?v=7BWWWQzTpNU")
 
 movies = [interstellar, black_panther,
-          get_out, shawshank_redemption, passengers]
+          django_unchained, shawshank_redemption, passengers]
+          
+# creates a webbpage with the movies we have chosen
 fresh_tomatoes.open_movies_page(movies)
